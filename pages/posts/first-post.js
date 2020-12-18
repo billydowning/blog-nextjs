e7eaba54eb7b037cec591a45d91e5d27fc136611
Billy Downing <wldowning@gmail.com>
2020-12-18 14:39:09 -0700

    layout for blog tutorial

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,25 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 class FirstPost extends React.Component {
   render() {
     return (
-      <div>
+      <Layout>
+        <Head>
+          <title>This is the first post</title>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+          />
+        </Head>
         <h1>First Post</h1>
         <h2>
           <Link href="/">
-            <a>Back to home</a>
+            <a className="ui primary button">Back to home</a>
           </Link>
         </h2>
-      </div>
+      </Layout>
     );
   }
 }
